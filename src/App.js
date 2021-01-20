41cf6fa11eb3ad13e28d82b62d4ddefd0888c3d6
abhi204655 <abhi9780253712@gmail.com>
2021-01-21 00:35:19 +0530

    done some cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import * as _api from "./api";
+import { saveAs } from "file-saver";
+import JSZip from "jszip";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [images, setImages] = useState(null);
+
+  const getImages = async () => {
+    console.log("entered");
+    let res = await _api.getImageSrc({ url });
+    setImages(res.data);
+  };
+
+  const saveFile = (fileUrl) => {
+    let fileName = fileUrl.split("/")[fileUrl.split("/").length - 1];
+    saveAs(fileUrl, fileName);
+  };
+
+  const saveAllFiles = async (urls) => {
+    let zip = new JSZip();
+    let folder = zip.folder("collection");
+
+    for (let i = 0; i < urls.length; i++) {
+      console.log(urls[i]);
+      const imageBlob = await fetch(urls[i]).then((response) =>
+        response.blob()
+      );
+      console.log(imageBlob);
+      let fileName = urls[i].split("/")[urls[i].split("/").length - 1];
+      const imageFile = new File([imageBlob], fileName);
+      folder.file(fileName, imageFile);
+    }
+
+    folder.generateAsync({ type: "blob" }).then((content) => {
+      console.log(content);
+      saveAs(content, "images");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="enter url"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button type="button" onClick={getImages}>
+        Get images
+      </button>
+      {images && (
+        <>
+          <button onClick={() => saveAllFiles(images)}>Download</button>
+          {images.map((i) => (
+            <div>
+              <img src={i} alt="downloadble" style={{ width: "50px" }} />
+              <button onClick={() => saveFile(i)}>Download</button>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+export const getImageSrc = async ({ url }) => {
+  try {
+    let res = await axios.post(
+      "https://assetsmania.herokuapp.com/api/get_images",
+      { url }
+    );
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
